b79c7ee22cfea8fd08700b0f262c1de75c6f7a7a
Ltdowl <sashag12s@gmail.com>
2022-08-20 22:24:51 +0300

    add

---
+++ models/contacts.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const addSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const joiShemas = {
+  add: addSchema,
+  updateFavorite: updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, joiShemas };
---
+++ routes/api/contacts.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/contacts");
+
+const router = express.Router();
+
+router.get("/", ctrl.getAll);
+
+router.get("/:contactId", ctrl.getById);
+
+router.post("/", ctrl.add);
+
+router.delete("/:contactId", ctrl.removeById);
+
+router.put("/:contactId", ctrl.updateById);
+
+router.patch("/:contactId/favorite", ctrl.updateStatusContact);
+
+module.exports = router;
