904f30ae6fb65a3c1e4ad0ff1dd90981b4a96715
Ltdowl <89732463+Ltdowl@users.noreply.github.com>
2022-09-10 14:30:21 +0300

    Update verifyEmail.js

--- controllers/users/verifyEmail.js
+++ controllers/users/verifyEmail.js
@@ -1,22 +1,31 @@
 const { basedir } = global;
 const { User } = require(`${basedir}/models/users`);
-const { createError } = require(`${basedir}/helpers/createError`);
 
 const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
 
   const user = await User.findOne({ verificationToken });
 
-  if (!user) {
-    throw createError(404);
-  }
-
-  await User.findByIdAndUpdate(user._id, {
-    verify: true,
-    verificationToken: null,
-  });
-
-  res.status(200).json({ status: 200, message: "Verification successful!" });
+     if (!user) {
+       return res
+         .status(404)
+         .json({
+           status: "error",
+           code: 404,
+           message: "User not found!"
+         });
+     }
+     await User.findByIdAndUpdate(user._id, {
+       verificationToken: null,
+       verify: true,
+     });
+     res
+       .status(200)
+       .json({
+         status: "success",
+         code: 200,
+         message: "Verification successful!",
+       });
 };
 
 module.exports = verifyEmail;
